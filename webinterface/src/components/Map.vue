8523a26ee5ef0f9246e3701d3e65b4f983ad073a
Jannick <jannick.schroer@gmail.com>
2023-12-08 15:09:57 +0100

    Switched to Composition

--- webinterface/src/components/Map.vue
+++ webinterface/src/components/Map.vue
@@ -1,27 +1,32 @@
 <script setup>
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiaXFpc215c2VucGFpIiwiYSI6ImNscHdtNHhnbzBoOGIybHA5cXB6OXBqYTgifQ.qvhDbZQkOTNCBqgSnKNANw';
-import { ref } from 'vue';
-import { MapboxMap } from '@studiometa/vue-mapbox-gl';
-
+import {ref, onMounted} from 'vue';
+import { createMap } from 'mapbox-composition';
 
 const mapCenter = ref([8.095723, 46.791793]);
+const map = ref(null);
+
+onMounted(() => {
+  const mapBox = createMap(map.value, {
+    style: 'mapbox://styles/mapbox/light-v11',
+    center: mapCenter.value,
+    zoom: 6.8,
+    minZoom: 6.8,
+    maxZoom: 12,
+    maxBounds: [[5.2559111595, 45.4179931641], [11.2920501709, 48.208380127]],
+    accessToken: MAPBOX_TOKEN,
+  });
+});
 </script>
 
 <template>
-  <MapboxMap
-    style="height: 500px"
-    :access-token="MAPBOX_TOKEN"
-    map-style="mapbox://styles/mapbox/light-v11"
-    :center="mapCenter"
-    :zoom="6.8"
-    :pitch="0"
-    :bearing="0"
-    :minZoom="6.8"
-    :maxZoom="13"
-    :maxBounds="[[5.2559111595, 45.4179931641], [11.2920501709, 48.208380127]]"
-  />
+  <div class="map" ref="map"></div>
 </template>
 
 <style scoped>
+.map {
+  height: 500px;
+  width: 100%;
+}
 
 </style>
