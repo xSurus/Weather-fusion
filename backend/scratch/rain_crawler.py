6c14ac0a4fd6525a9886b493b4f35ba2d95a1d9c
AliSot2000 <alisot200@gmail.com>
2023-12-12 15:31:04 +0100

    + Added get_next_prediction -> Checks if there are new predictions available + update_prediction -> Fetches the new prediction values and writes them to disk, removes the old prediction from disk and from db + craw_prediction -> Searches through the predictions and updates them if the output directory changed

--- backend/scratch/rain_crawler.py
+++ backend/scratch/rain_crawler.py
@@ -55,7 +55,121 @@ def init_db(db_path: str):
     rdb.cleanup()
 
 
-def crawl_radar():
+# Better idea https://www.meteoswiss.admin.ch/product/output/versions.json
+def get_next_prediction(dt: datetime.datetime):
+    """
+    Find the next prediction output if it exists, otherwise return the current one
+    """
+    # Get now and store the last version when we started
+    now = datetime.datetime.now()
+    last_version = dt
+
+    # Walk forward in time until we find the next production and next prediction
+    while dt < now:
+        next_prediction = dt - datetime.timedelta(minutes=dt.minute % 5) + datetime.timedelta(minutes=5)
+
+        st, js = request_prediction_data(next_prediction, dt)
+        if st == 200:
+            last_version = dt
+
+        dt += datetime.timedelta(minutes=1)
+
+    return last_version
+
+
+def update_prediction(version: datetime.datetime, update_time: datetime.datetime):
+    """
+    Update the prediction data
+    """
+    # Create new directory
+    prediction_path = os.path.join(data_home, f"prediction_{version.strftime('%Y%m%d_%H%M')}")
+    os.makedirs(prediction_path, exist_ok=True)
+
+    db_path = os.path.join(data_home, "rain.db")
+
+    # Check if the db exists and init otherwise
+    if not os.path.exists(db_path):
+        init_db(db_path)
+
+    rdb = RainDB(db_path)
+
+    # Get the prediction for as long as they come and insert into the database
+    while True:
+        next_prediction = update_time - datetime.timedelta(minutes=version.minute % 5) + datetime.timedelta(minutes=5)
+
+        st, js = request_prediction_data(next_prediction, version)
+
+        # Successful request
+        if st == 200:
+
+            assert js is not None, "js is None from meteoswiss prediction response"
+            store_path = os.path.join(prediction_path, f"{next_prediction.strftime('%Y%m%d_%H%M')}.json")
+
+            # Transform the MeteoData to GeoJSON
+            transformed = decode_geojson(js)
+
+            # Write to File
+            with open(store_path, "w") as f:
+                json.dump(transformed, f)
+
+            rdb.insert_prediction_entry(next_prediction, version)
+
+        else:
+            break
+
+    # Create new symlink
+    os.symlink(prediction_path, os.path.join(data_home, "prediction"))
+
+    # Remove old prediction from database
+    last_prediction = rdb.remove_last_prediction()
+
+    # If the last prediction is present, we need to remove the directory
+    if last_prediction is not None:
+        old_prediction_path = os.path.join(data_home, f"prediction_{last_prediction.strftime('%Y%m%d_%H%M')}")
+
+        # If the old prediction exists, we delete it.
+        if os.path.exists(old_prediction_path) and os.path.isdir(old_prediction_path):
+            print("Deleted old path")
+            shutil.rmtree(old_prediction_path)
+
+    # Commit database
+    rdb.cleanup()
+
+
+def crawl_prediction(update_time: datetime.datetime):
+    """
+    Crawl Radar Prediction.
+    """
+    cfg_path = os.path.join(data_home, "config.json")
+
+    # Parse the config or create a new one
+    if os.path.exists(cfg_path):
+        with open(cfg_path, "r") as f:
+            cfg = json.load(f)
+
+    else:
+        dt = datetime.datetime.now() - datetime.timedelta(days=1)
+        cfg = {
+            "latest_prediction": datetime.
+            datetime(year=dt.year, month=dt.month, day=dt.day, hour=0, minute=0, second=0).strftime("%Y-%m-%d %H:%M:%S")
+        }
+
+    old_prediction = datetime.datetime.strptime(cfg["latest_prediction"], "%Y-%m-%d %H:%M:%S")
+
+    # check the next prediction
+    new_version = get_next_prediction(old_prediction)
+
+    # Update config if it has changed
+    if new_version != old_prediction:
+        cfg["latest_prediction"] = new_version.strftime("%Y-%m-%d %H:%M:%S")
+
+        with open(cfg_path, "w") as f:
+            json.dump(cfg, f)
+
+        update_prediction(version=new_version, update_time=update_time)
+
+
+def crawl_radar(update_time: datetime.datetime):
     """
     Crawl the radar data.
     """
