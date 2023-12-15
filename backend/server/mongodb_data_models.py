c96dd5873711a6bab81b36f61424fe749e258dc5
AliSot2000 <alisot200@gmail.com>
2023-12-15 12:45:56 +0100

    + Added RecordTpe for wind + Added WindRecord

--- backend/server/mongodb_data_models.py
+++ backend/server/mongodb_data_models.py
@@ -4,15 +4,27 @@ from typing import Union
 from enum import Enum
 
 
-class RecordType(str, Enum):
-    owner = "init"
-    member = "radar"
-    invited = "prediction"
+class RainRecordType(str, Enum):
+    radar = "radar"
+    prediction = "prediction"
+
+
+class WindRecordType(str, Enum):
+    strength = "strength"
+    direction = "direction"
 
 
 class RainRecord(BaseModel):
     record_id: Union[None, str] = Field(alias='_id', default=None)
     dt: datetime.datetime
-    type: RecordType
+    type: RainRecordType
+    version: Union[None, datetime.datetime] = None
+    processed: bool = False
+
+
+class WindRecord(BaseModel):
+    record_id: Union[None, str] = Field(alias='_id', default=None)
+    dt: datetime.datetime
+    type: WindRecordType
     version: Union[None, datetime.datetime] = None
     processed: bool = False
