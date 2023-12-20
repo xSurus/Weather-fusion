97cf08ddc6c95a90e766ef600510503f109c5811
AliSot2000 <alisot200@gmail.com>
2023-12-20 22:27:09 +0100

    + Clear Database

--- backend/scratch/mongodb_test.py
+++ backend/scratch/mongodb_test.py
@@ -1,4 +1,4 @@
-from server.mongo_db_api import MongoAPI
+from server.mongo_db_api import MongoAPI, object_id_to_string
 from server.config import ServerConfig
 import os
 import json
@@ -18,11 +18,50 @@ with open(config_path, "r") as f:
     mongo = MongoAPI(db_address=server_config.mongo_db.address, db_name=server_config.mongo_db.database,
                      db_username=server_config.mongo_db.username, db_password=server_config.mongo_db.password)
 
-record = RainRecord(
-    type="radar",
-    dt=datetime.datetime.now(),
-)
-dtc = record.model_dump()
-del dtc["record_id"]
+all_data = mongo.find(collection="danger_data", filter_dict={})
+print(len(all_data))
+rm_count = 0
+del_count = 0
+for d in all_data:
+    path = os.path.join(data_home, "storage", object_id_to_string(d['_id']) + ".json")
+    if os.path.exists(path):
+        os.remove(path)
+        rm_count += 1
 
-mongo.insert_one(collection="rain_data", document_dict=dtc)
+    del_count += mongo.delete_one(collection="danger_data", filter_dict={"_id": d['_id']})
+
+print(f"Removed {rm_count} files and {del_count} database entries for danger_data")
+
+all_data = mongo.find(collection="wind_data", filter_dict={})
+print(len(all_data))
+rm_count = 0
+del_count = 0
+for d in all_data:
+    if d['type'] == "strength":
+        path = os.path.join(data_home, "storage", object_id_to_string(d['_id']) + ".json")
+    elif d['type'] == "direction":
+        path = os.path.join(data_home, "storage", object_id_to_string(d['_id']) + ".png")
+    else:
+        print("Unknown type: ", d['type'])
+        continue
+    if os.path.exists(path):
+        os.remove(path)
+        rm_count += 1
+
+    del_count += mongo.delete_one(collection="wind_data", filter_dict={"_id": d['_id']})
+
+print(f"Removed {rm_count} files and {del_count} database entries for wind_data")
+
+all_data = mongo.find(collection="rain_data", filter_dict={})
+print(len(all_data))
+rm_count = 0
+del_count = 0
+for d in all_data:
+    path = os.path.join(data_home, "storage", object_id_to_string(d['_id']) + ".json")
+    if os.path.exists(path):
+        os.remove(path)
+        rm_count += 1
+
+    del_count += mongo.delete_one(collection="rain_data", filter_dict={"_id": d['_id']})
+
+print(f"Removed {rm_count} files and {del_count} database entries for rain_data")
