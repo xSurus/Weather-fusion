42c88a9e54551564dd60855ff92cf01e4b494902
AliSot2000 <alisot200@gmail.com>
2024-01-02 11:58:30 +0100

    + Different port

--- backend/main.py
+++ backend/main.py
@@ -3,4 +3,4 @@ from server.api import app
 import os
 
 logging_config = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "logging.yaml")
-uvicorn.run(app, host="localhost", port=8000, log_config=logging_config)
+uvicorn.run(app, host="localhost", port=8001, log_config=logging_config)
